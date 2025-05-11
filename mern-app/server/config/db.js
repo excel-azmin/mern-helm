554584b6d5bcb5b061c01ce0951f8043deb44f99
Shaid Azmin <excel.azmin@gmail.com>
2025-05-11 14:02:23 +0600

    init full project

---
+++ mern-app/server/config/db.js
@@ -0,0 +1,17 @@
+require("dotenv").config(); // Load environment variables from .env file
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+// Use the DB_URL from the .env file
+const dbUrl = process.env.DB_URL;
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
